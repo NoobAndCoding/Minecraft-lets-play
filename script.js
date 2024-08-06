071807f40fb1a43c4e9a62260ad88d692b038b2e
NoobAndCoding <just.gaming554t644@gmail.com>
2024-08-06 14:59:45 +0300

    fixes + added things :D

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-
+function myFunction() {
+    document.getElementById('myButton').addEventListener('click', function() {
+        var element = document.getElementById('sidebar');
+        if (element.style.opacity === '0') {
+        element.style.opacity = '1';
+        } else {
+        element.style.opacity = '0';
+        }
+    });
+}
 
